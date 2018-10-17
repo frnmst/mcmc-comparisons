715000226f11f1127281739356a51185986995d4
Franco Masotti <franco.masotti@live.com>
2018-10-17 13:06:46 +0200

    Started writing a basic loop.

--- poc.pl
+++ poc.pl
@@ -16,25 +16,38 @@
 main :-
     current_prolog_flag(argv, Argv),
     format('Called with ~q\n', [Argv]),
+    format('# samples\tmhs_time\tgbs_time\n'),
     format('\n'),
     append(_, [X], Argv),
-    atom_number(X, Samples),
-    measure_mh(Time_mh,Samples),
-    measure_gibbs(Time_gibbs,Samples),
-    format('# samples\tmhs_time\tgbs_time\n'),
-    format('~q\t~q\t~q\n', [Samples, Time_mh, Time_gibbs]),
+    atom_number(X, Min),
+    append(_, [Y], Argv),
+    atom_number(Y, Step),
+    loop(Min,Step),
     halt.
 
 main :-
     halt(1).
 
-measure_mh(Time, Samples):-
+/* loop(max,_) */
+loop(N,_):-
+    N>=26,
+    !.
+
+loop(N, Step):-
+    Samples is N,
+    measure_mh_arithm(Time_mh,Samples),
+    measure_gibbs_arithm(Time_gibbs,Samples),
+    format('~q\t~q\t~q\n', [Samples, Time_mh, Time_gibbs]),
+    N1 is N+Step,
+    loop(N1,Step).
+
+measure_mh_arithm(Time, Samples):-
     [swish/examples/inference/arithm],
     statistics(walltime, [_|[_]]),
     mc_mh_sample(eval(2,4),eval(1,3),Samples,_,[mix(100),lag(3),successes(_),failures(_)]),
     statistics(walltime, [_|[Time]]).
 
-measure_gibbs(Time, Samples):-
+measure_gibbs_arithm(Time, Samples):-
     [swish/examples/inference/arithm],
     statistics(walltime, [_|[_]]),
     mc_gibbs_sample(eval(2,4),eval(1,3),Samples,_,[mix(100),lag(3),successes(_),failures(_)]),
